c9fd4d79be00f4232dac262d05101457c16c2709
semssk <ssksem015@gmail.com>
2023-05-13 00:01:06 +0100

    Added routing

--- EDL/src/App.svelte
+++ EDL/src/App.svelte
@@ -1,9 +1,16 @@
 <script>
+  import { Router, Route } from "svelte-navigator";
   import AdminPage from "./component/admin/AdminPage.svelte";
   import Login from "./component/general/login/login.svelte";
 </script>
 
 <main>
-  <Login />
-  <!-- <AdminPage /> -->
+  <Router>
+    <Route path="login">
+      <Login />
+    </Route>
+    <Route path="admin">
+      <AdminPage />
+    </Route>
+  </Router>
 </main>
--- EDL/src/component/admin/AdminPage.svelte
+++ EDL/src/component/admin/AdminPage.svelte
@@ -4,12 +4,6 @@
   import SideBar from "./SideBar.svelte";
 </script>
 
-<SideBar />
-<section id="content">
-  <NavBar />
-  <Main />
-</section>
-
 <svelte:head>
   <link
     href="https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css"
@@ -19,11 +13,17 @@
     rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
   />
-  <link rel="stylesheet" href="public/css/AdminPage.css" />
+  <link rel="stylesheet" href="/css/AdminPage.css" />
   <script defer src="https://www.gstatic.com/charts/loader.js"></script>
   <script defer src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
   <script defer src="js/AdminPage.js"></script>
   <!-- <script defer src="chart1.js"></script> -->
 </svelte:head>
+
+<SideBar />
+<section id="content">
+  <NavBar />
+  <Main />
+</section>
 
 <style src="assets/adminpage.css"></style>

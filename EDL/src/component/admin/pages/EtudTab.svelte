5407ecb8d1c07e546652860e41d61394d9370af2
wassim2001lab <mohamed.labraoui@univ-constantine2.dz>
2023-05-20 22:28:34 +0100

    separated foorm from popup

--- EDL/src/component/admin/pages/EtudTab.svelte
+++ EDL/src/component/admin/pages/EtudTab.svelte
@@ -108,45 +108,7 @@
     <!-- end of form modal -->
 
     <!-- address book home -->
-    <div class = "addr-book" id = "addr-book">
-        <div class = "addr-book-content">
-            <div class = "addr-book-head">
-                <i class = "fa-solid fa-address-book fa-2x"></i>
-                <h2>Address<span>Book</span></h2>
-            </div>
-
-            <div class = "addr-book-top">
-                <button type = "button" class = "btn" id = "add-btn">
-                    <span><i class = "fas fa-plus"></i> Add address</span>
-                </button>
-            </div>
-
-            <div class = "addr-book-list" id = "addr-book-list">
-                <table class = "addr-table">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Address</th>
-                            <th>Labels</th>
-                            <th>By Name</th>
-                            <th>Phone</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <!-- <tr>
-                            <td>1</td>
-                            <td>
-                                <span class = "addressing-name">Smith Family</span><br><span class = "address">Johnson Street - 5668, Lake side</span>
-                            </td>
-                            <td><span>Colleagues</span></td>
-                            <td>Mark Smith</td>
-                            <td>123-456-7890</td>
-                        </tr> -->
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
+  
     <!-- end of address book home -->
 
 </main>
